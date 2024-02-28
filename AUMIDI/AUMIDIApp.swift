1d106d4c13056b7a8c89ed084be161e81aed5026
iamhabi <iamhabi@pm.me>
2024-02-28 22:55:44 +0900

    Initial Commit

---
+++ AUMIDI/AUMIDIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AUMIDIApp.swift
+//  AUMIDI
+//
+//  Created by habi on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct AUMIDIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AUMIDI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AUMIDI
+//
+//  Created by habi on 2/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
